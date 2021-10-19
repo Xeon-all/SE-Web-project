6fef5ec663d11b99fd682fee7368e06ec6c64154
Yu Runfei <1900011033@pku.edu.cn>
2021-10-19 22:30:48 +0800

    basic layout && basic add task function

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,62 @@
 import {
   Button,
-  Modal,
+  Card,
+  Layout,
 } from 'antd'
-import { useState } from 'react'
+import {
+  PlusCircleOutlined,
+  MinusSquareOutlined,
+} from '@ant-design/icons'
+import { useState, useReducer } from 'react'
 import './App.css';
 
+const { Header, Content, Sider } = Layout;
+
 function App() {
-  const [ModalVisible, setModalVisible] = useState(false);
+  const [Tasks, setTasks] = useState([]);
+  const [ignored, forceUpdate] = useReducer(x => x + 1, 0);
+
+  function handleAdd() {
+    let newTask = {
+      id: Tasks.length,
+      key: 2,
+    }
+    let task = [...Tasks, newTask];
+    setTasks(task);
+  }
+
+  function handleDelete(id) {
+    let task = Tasks;
+    console.log(task[id]);
+    task.splice(id, 1);
+    setTasks(task);
+    forceUpdate();
+  }
+
   return (
     <>
-      <Button
-      onClick = {() => {setModalVisible(true)}}
-      style = {{transform: 'translate(50%, 50%)'}}>
-        Hello, world!
-      </Button>
-      <Modal
-      visible = {ModalVisible}
-      footer = {null}
-      onCancel = {() => {setModalVisible(false)}}>
-        <h1>Hello, world!</h1>
-      </Modal>
+    <Layout>
+      <Header>
+        <Button style = {{marginLeft: '50%'}} onClick = {handleAdd}>
+          add
+        </Button>
+      </Header>
+      <Layout>
+        <Sider style = {{background: '#49C7CD', minHeight: '90vh'}} collapsible></Sider>
+        <Content>
+          {Tasks.map((val) => {
+            return (
+              <Card style = {{margin: '20px'}}>
+                <MinusSquareOutlined onClick = {() => {handleDelete(val.id)}}/>
+                {`第${val.id}个任务`}
+                <PlusCircleOutlined style = {{float: 'right'}}/>
+              </Card>
+            )
+          })}
+        </Content>
+        <Sider style = {{background: '#49C7CD', minHeight: '90vh'}} collapsible></Sider>
+      </Layout>
+    </Layout>
     </>
   );
 }
